5ef9f551c78f708627e61de53b0472dfe04c0251
xiexin12138 <xiexin344@163.com>
2025-06-21 16:36:18 +0800

    update

--- app/dashboard/customers/page.tsx
+++ app/dashboard/customers/page.tsx
@@ -1,3 +1,3 @@
 export default async function Page() {
-  return <p>Customers Page</p>;
+  return <p>Customers Page.</p>;
 }
--- app/dashboard/invoices/page.tsx
+++ app/dashboard/invoices/page.tsx
@@ -1,3 +1,3 @@
 export default async function Page() {
-  return <p>Invoices Page</p>;
+  return <p>Invoices Page.</p>;
 }
